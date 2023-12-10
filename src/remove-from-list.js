6a2829a274727d6f30dc929c72c60d633b6de250
yix_x_xio <iivan.krulov@mail.ru>
2023-12-10 18:14:37 +0300

    Update remove-from-list.js
    Remove list complete!

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -19,7 +19,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  */
 
-module.exports = function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+
+module.exports = function removeKFromList(l , k)
+{
+  
+    if (l === null) return null;
+    
+    if (l.value === k) {
+      return removeKFromList(l.next, k);
+    }
+    
+    l.next = removeKFromList(l.next, k);
+    
+    return l;
+  
+  
+  }  
