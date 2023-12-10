b11e4976b1f692af57a33eef2e1c9d9cb68cad9d
yix_x_xio <iivan.krulov@mail.ru>
2023-12-10 18:10:02 +0300

    Task
    Stack complete!

--- src/stack.js
+++ src/stack.js
@@ -1,30 +1,49 @@
-const { NotImplementedError } = require("../extensions/index.js");
+const { NotImplementedError } = require("../extensions/index.js"); 
+ 
+/** 
+ * Implement the Stack with a given interface via arraloy. 
+ * 
+ * @example 
+ * const stack = new Stack(); 
+ * 
+ * stack.push(1); // adds the element to the stack 
+ * stack.peek(); // returns the peek, but doesn't delete it, returns 1 
+ * stack.pop(); // returns the top element from stack and deletes it, returns 1 
+ * stack.pop(); // undefined 
+ * 
+ */ 
+ 
+ 
+module.exports =class Stack { 
+ 
+constructor() 
+{ 
+  this.data =[]; 
+} 
+ 
+ 
+  push(value) { 
+    this.data.push(value); 
+    
+  } 
+ 
+  pop() { 
+   return this.data.pop(); 
+  } 
+ 
+  peek() { 
+  return this.data[this.data.length-1]; 
+  } 
+ 
+  size() 
+  { 
+    return this.data.length; 
+  } 
+}; 
+ 
+/*const stack = new Stack(); 
 
-/**
- * Implement the Stack with a given interface via array.
- *
- * @example
- * const stack = new Stack();
- *
- * stack.push(1); // adds the element to the stack
- * stack.peek(); // returns the peek, but doesn't delete it, returns 1
- * stack.pop(); // returns the top element from stack and deletes it, returns 1
- * stack.pop(); // undefined
- *
- */
-module.exports = class Stack {
-  push(/* element */) {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
-  }
+stack.push('jalov'); 
+stack.push('bunny'); 
 
-  pop() {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
-  }
-
-  peek() {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
-  }
-};
+console.log(stack);*/
