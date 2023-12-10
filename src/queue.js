bba27058b5827e449ca6310aeaf6362b21cbc8a4
yix_x_xio <iivan.krulov@mail.ru>
2023-12-10 18:16:14 +0300

    Update queue.js
    queue complete

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,8 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
+const a = new ListNode();
+a.
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -13,21 +15,86 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-module.exports = class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+module.exports =class Queue {
+  constructor()
+  {
+    this.head = null;
+    this.tail = null;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+ 
+
+  getUnderlyingList() {
+    
+   return this.head;
+   
+  }
+
+  enqueue(value) {
+    const newNode = {value, next : null};
+     if(this.is_Empty())
+     {
+      this.head = newNode;
+      this.tail = newNode;
+
+     }
+     else
+     {
+      this.tail.next = newNode;
+      this.tail= newNode;
+     }
+
+  
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if(this.is_Empty())
+    {
+      return null;
+    }
+
+    const dequeued_Node = this.head;
+    this.head = this.head.next;
+
+    if(this.is_Empty())
+    {
+      this.tail=null;
+    }
+
+    return dequeued_Node.value;
+
+
+  
+  }
+
+  is_Empty()
+  {
+    return !this.head;
   }
 
 }
+
+
+/*const obj = new Queue();
+obj.enqueue(1337);
+obj.enqueue('popa');
+obj.enqueue('pisya');
+obj.enqueue('gggg');
+obj.enqueue(228);
+
+console.log(obj.getUnderlyingList());*/
+
+/*for (; obj.head!=null; ) 
+  {
+ 
+    console.log(obj.dequeue());
+  }*/
+
+  
+
+
+
+
+
+
